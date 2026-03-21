b116888d4256254654ec6a4190131d1efd177a30
kylanli <kylanyli20@gmail.com>
2026-03-21 16:35:47 -0400

    working version test_auth.py

--- backend/tests/test_auth.py
+++ backend/tests/test_auth.py
@@ -1,9 +1,3 @@
-import sys
-import os
-
-# Add the backend directory to the Python path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
-
 from fastapi import HTTPException
 from backend.src.auth import verify_token
 
